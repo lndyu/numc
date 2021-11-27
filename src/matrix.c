b403f9d700a6c18134de7f9b092fd835148fa24c
Leonard Yu <leoyumail2000@gmail.com>
2021-11-26 21:09:53 -0800

    yes

--- src/matrix.c
+++ src/matrix.c
@@ -304,28 +304,44 @@ int pow_matrix(matrix *result, matrix *mat, int pow) {
     allocate_matrix(&temp_mat,result->cols,result->rows);
     fill_matrix(temp_mat,0);
     fill_matrix(result,0);
+#pragma omp parallel
+    {
+#pragma omp for
     for(int i = 0; i < result->cols; i++){
 	result->data[i+i*result->cols] = 1;
+    }
     }
     if(pow == 0){
 	return 0;
     }
     int current = 1;
+#pragma omp parallel
+    {
+#pragma omp for
     for(int i = 0; i < mat->rows*mat->cols;i++){
 	result->data[i] = mat->data[i];
+    }
     }
     while(current * 2 <= pow){
 	current *= 2;
 	mul_matrix(temp_mat,result,result);
+#pragma omp parallel
+	{
+#pragma omp for
 	for(int i = 0; i<mat->rows*mat->cols;i++){
 	    result->data[i] = temp_mat->data[i];
 	}
+	}
     }
+#pragma omp parallel
+    {
+#pragma omp for
     for(int i = current;i<pow;i++){
 	mul_matrix(temp_mat,mat,result);
 	for(int j = 0; j < mat->rows * mat-> cols;j++){
 	    result->data[j] = temp_mat->data[j];
 	}
+    }
     }
     free(temp_mat);
     return 0;
